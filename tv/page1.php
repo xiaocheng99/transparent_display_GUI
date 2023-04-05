b3c9d095ea51654e7c7d3da3747846dd6c972b2d
xiaocheng99 <56629576+xiaocheng99@users.noreply.github.com>
2023-04-05 14:17:19 +0800

    网页前端代码

---
+++ tv/page1.php
@@ -0,0 +1,9 @@
+
+<?php
+$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+$msg = '1';
+$len = strlen($msg);
+socket_sendto($sock, $msg, $len, 0, '42.192.164.131',1520);
+header("Location: http://helloqt.top/tv");
+socket_close($sock);
+?>
